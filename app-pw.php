ccd86955cd5fd945b448d531cee4aac255a0c298
sbm12 <seth@wandr.me>
2024-11-19 17:06:46 -0500

    AppPW warn file - initial build

---
+++ app-pw.php
@@ -0,0 +1,12 @@
+<hr />
+<h2 style='color:red'>DO NOT USE YOUR REGULAR PASSWORD HERE</h2>
+<p>Create a new <a href="https://bsky.app/settings/app-passwords" target="_blank">App Password</a></p>
+<ul>
+    <li>Click this link: <a href="https://bsky.app/settings/app-passwords" target="_blank">App Password</a></li>
+    <li>Click Add App Password</li>
+    <li>Give it a name like list converter or something so you know what it is</li>
+    <li>Click Create App Password</li>
+    <li>Copy the string it comes back with and use that as your password here</li>
+    <li>When you are done with whatever List/Starter Pack stuff you're doing, delete the App Password, just in case</li>
+</ul>
+<hr />
